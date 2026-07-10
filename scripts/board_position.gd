0efb3a14191bb2ae87126d3bb389ba79b9ebd96c
Leonard Fäcke <l.faecke@stud.uni-goettingen.de>
2026-07-10 16:04:54 +0200

    Removed board collision, added basic board position scene

---
+++ scripts/board_position.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+################################################################################
+# This scene represents a possible position on the board. They should be placed
+# manually underneath the board and stored in a graph in the main game.
+################################################################################
+
+@export var event: Script
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

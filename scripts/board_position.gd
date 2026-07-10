7fe2c014df9f04e783e139f3e916eec816bfe281
Leonard Fäcke <l.faecke@stud.uni-goettingen.de>
2026-07-10 16:23:53 +0200

    Added basic positioning with decentralized graph

--- scripts/board_position.gd
+++ scripts/board_position.gd
@@ -6,6 +6,7 @@ extends Node3D
 ################################################################################
 
 @export var event: Script
+@export var neighbors: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
---
+++ scripts/game.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var current_position: Node3D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current_position = find_child("start_pos", false)
+	if current_position == null:
+		print("FUCK")
+	else:
+		print(current_position.position)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

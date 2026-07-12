7453b43867583a0d4546ff78ba9ee45ed2c3c93a
Kerstin Hoffmann <kerstinhoffmann02@outlook.de>
2026-07-12 12:20:06 +0200

    incorporated pieces

--- scripts/movement_arrows.gd
+++ scripts/movement_arrows.gd
@@ -2,27 +2,6 @@ extends Node3D
 
 @export var destination: NodePath
 
-var grow = true
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if grow:
-		scale *= 1.005
-	else:
-		scale *= 0.995
-		
-	if scale >= Vector3(1, 1, 1) * 1.5:
-		grow = false
-	
-	if scale <= Vector3(1, 1, 1):
-		grow = true
-
-
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton \
 			and event.button_index == MOUSE_BUTTON_LEFT \
